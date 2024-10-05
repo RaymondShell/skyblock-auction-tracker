b6388ee38cc06eb047b45d56a3a02f49dfcb112d
RaymondShell <alphamag0110@gmail.com>
2024-10-05 12:55:22 +0100

    goes through all the pages asynchronously and fetches majority of auctions successfully

---
+++ src/structs.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Auctions {
+    pub totalPages: u32,
+    pub page: i32,
+    pub auctions: Vec<Auction>,
+    pub totalAuctions: u32,
+
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Auction {
+    pub uuid: String,
+    pub item_name: String,
+    pub category: String,
+    pub tier: String,
+    pub starting_bid: u32,
+    pub item_bytes: String,
+    pub bin: bool,
+
+}
